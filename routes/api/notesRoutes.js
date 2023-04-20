4f36d619b464b5f5969dd27097fae0b75dead4e4
Brosa <brunoarosa1997@gmail.com>
2023-04-20 17:10:11 -0400

    updated post route to read, update and write a db.json file

--- routes/api/notesRoutes.js
+++ routes/api/notesRoutes.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 const fs = require("fs");
+const {
+  writeToFile,
+} = require("../../../GT-VIRT-FSF-PT-01-2023-U-LOLC/11-Express/01-Activities/28-Stu_Mini-Project/Main/helpers/fsUtils");
 
 router.get("/", (req, res) => {
   const data = fs.readFile("./db/db.json", "utf8", (err, data) => {
@@ -7,12 +10,29 @@ router.get("/", (req, res) => {
       console.error(err);
       return;
     }
+    console.log("Data Received")
     res.json(JSON.parse(data));
   });
 });
 
-router.post("/", (req,res) => {
-    console.log(req.body)
-})
+router.post("/", (req, res) => {
+  const data = fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("file read")
+    const parsedData = JSON.parse(data);
+    parsedData.push(req.body);
+    const newData =JSON.stringify(parsedData)
+    fs.writeFile("./db/db.json", newData, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    console.log("New data added")
+    });
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const PORT = 3001;
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 app.use(routes);
 
 app.listen(PORT, () =>
