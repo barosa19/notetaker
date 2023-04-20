e87ba25df56e7ce3dfe2458f1fa488b04f9d6be9
Brosa <brunoarosa1997@gmail.com>
2023-04-20 18:36:02 -0400

    worked on cleaning api/notes get and post routes

--- routes/api/notesRoutes.js
+++ routes/api/notesRoutes.js
@@ -1,17 +1,14 @@
 const router = require("express").Router();
 const fs = require("fs");
-const {
-  writeToFile,
-} = require("../../../GT-VIRT-FSF-PT-01-2023-U-LOLC/11-Express/01-Activities/28-Stu_Mini-Project/Main/helpers/fsUtils");
 
 router.get("/", (req, res) => {
-  const data = fs.readFile("./db/db.json", "utf8", (err, data) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
       console.error(err);
       return;
     }
-    console.log("Data Received")
-    res.json(JSON.parse(data));
+    console.log("Data Received");
+    return res.json(JSON.parse(data))
   });
 });
 
@@ -21,16 +18,17 @@ router.post("/", (req, res) => {
       console.error(err);
       return;
     }
-    console.log("file read")
+    console.log("file read");
     const parsedData = JSON.parse(data);
     parsedData.push(req.body);
-    const newData =JSON.stringify(parsedData)
+    const newData = JSON.stringify(parsedData);
     fs.writeFile("./db/db.json", newData, (err) => {
       if (err) {
         console.error(err);
         return;
       }
-    console.log("New data added")
+      console.log("New data added");
+      return res.json(newData)
     });
   });
 });
